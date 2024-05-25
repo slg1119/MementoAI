d2c7602ae8148d8cff0fa779c0ceacd49d650cda
RokHyung Son <fhrgud1264@gmail.com>
2024-05-26 04:35:44 +0900

    feat: shorten_url을 생성하는 기능 추가

--- app/main.py
+++ app/main.py
@@ -1,13 +1,30 @@
-from fastapi import FastAPI
+import uuid
+
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from starlette import status
+from starlette.responses import RedirectResponse
+
+from . import repository, model, schema
+from .database import SessionLocal, engine
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/shorten", response_model=schema.UrlCreateResponse)
+async def create_shorten_url(
+    url: schema.UrlCreateRequest, db: Session = Depends(get_db)
+):
+    create: schema.UrlCreate = schema.UrlCreate(
+        url=url.url, short_url=str(uuid.uuid4())
+    )
+    db_url = repository.create_shorten_url(db, create)
+    return schema.UrlCreateResponse(short_url=db_url.short_url)
